59a6207435ae97b96f031a0697cd679022a669aa
Bernardo <costalopesbernardo@gmail.com>
2025-08-01 16:53:07 -0300

    arquivos 01/08

---
+++ Fundamentos/operadores_aritmeticos.py
@@ -0,0 +1,20 @@
+#adição
+print(1 + 1)
+
+#subtração
+print(10 - 3)
+
+#multiplicação
+print(4*80)
+
+#divisão 
+print(10/2)
+
+#divisão inteira - só considera a parte inteira
+print(11//2)
+
+#módulo - pega o resto da divisão  
+print(10 % 3)
+
+#exponeciação 
+print( 2**3 )
---
+++ operadores_comparacao.py
@@ -0,0 +1,27 @@
+#igualdade
+saldo = 400
+saque = 200
+
+print(saldo == saque)
+
+#diferença
+saldo = 400
+saque = 200
+
+print(saldo != saque)
+
+
+#maior que / maior ou igual que
+saldo = 400
+saque = 200
+
+print(saldo > saque)
+print(saldo >= saque)
+
+
+#menor que / menor ou igual que
+saldo = 400
+saque = 200
+
+print(saldo < saque)
+print(saldo <= saque)
