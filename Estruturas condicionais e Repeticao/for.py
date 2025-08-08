f9ea1ff6328179b9040bae43d5058b09a371fd0e
Bernardo <costalopesbernardo@gmail.com>
2025-08-07 21:59:56 -0300

    novo

--- Estruturas condicionais e Repeticao/for.py
+++ Estruturas condicionais e Repeticao/for.py
@@ -1,6 +1,6 @@
 # soma = 0
 
-# for i in range(0,10):   #esse código vai executar o loop começando do 0 e indo até o 10 (sempre o último                valor -1), esse range() indica até qual o intervalo de valores o loop irá executar, nesse caso "range(0, 10) o loop começa no 0 e termina no 9 " 
+# for i in range(0,10):   #! esse código vai executar o loop começando do 0 e indo até o 10 (sempre o último                valor -1), esse range() indica até qual o intervalo de valores o loop irá executar, nesse caso "range(0, 10) o loop começa no 0 e termina no 9 " 
     
 #     soma += i
 #     print(soma)
@@ -19,7 +19,7 @@
 
 # frutas = ['banana', 'abacate', 'uva', 'pera']
 
-# for sequencia_fruta in frutas:               # a variavel "sequencia_fruta" vai armazenar cada valor da repetição, em que o "in" vai indicar em qual lista ou variavel com valores o laço deve buscar para realizar o loop 
+# for sequencia_fruta in frutas:               #! a variavel "sequencia_fruta" vai armazenar cada valor da repetição, em que o "in" vai indicar em qual lista ou variavel com valores o laço deve buscar para realizar o loop 
 #     print(sequencia_fruta)
  
 for i in range(0,11):
